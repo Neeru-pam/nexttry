8bfb3932e3705526b54a809957ad442eedbc31dd
Neerupam <neerupam.26@gmail.com>
2025-09-15 07:29:11 +0530

    change path

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://nexttry-k9st.onrender.com/',
     credentials:true
 };
 app.use(cors(corsOption)); 
